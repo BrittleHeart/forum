c7468ed9ba7fb893c280a7c1f719ef360ef60bc0
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-15 21:19:37 +0100

    Added helmet, cors and morgan middleware :rocket:

--- main-server/src/app.module.ts
+++ main-server/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(helmet(), morgan('dev')).forRoutes('/')
+  }
+}
