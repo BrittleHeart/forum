efc4bc08dbac4b04bfc86eb492edf795c8692907
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-16 17:17:37 +0100

    Added soft deletes for user.service

--- main-server/src/users/users.service.ts
+++ main-server/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { QueryService } from '@nestjs-query/core';
+import { TypeOrmQueryService } from '@nestjs-query/query-typeorm';
 
 @Injectable()
-export class UsersService {}
+@QueryService(UserEntity)
+export class UsersService extends TypeOrmQueryService<UserEntity> {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {
+    super(userRepository, { useSoftDelete: true });
+  }
+}
