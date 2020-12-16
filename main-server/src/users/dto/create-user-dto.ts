125c1f00c1bdcdc7c189947e3f7dc1874c4eb79c
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-16 17:36:41 +0100

    Created user's dto

--- main-server/src/users/dto/create-user-dto.ts
+++ main-server/src/users/dto/create-user-dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Max, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Min(3)
+  @Max(45)
+  readonly name: string;
+
+  @IsEmail()
+  @Min(7)
+  @Max(255)
+  readonly email: string;
+
+  @IsString({ message: 'Password must be an string' })
+  @Min(8, { message: 'Password must be at least 8 characters long' })
+  @Max(255, { message: 'Password length must me less or equal 255' })
+  readonly password;
+}
--- main-server/src/users/dto/update-user-dto.ts
+++ main-server/src/users/dto/update-user-dto.ts
@@ -1,1 +1,13 @@
-export class UpdateUserDto {}
+import { IsString, Max, Min } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @Min(3)
+  @Max(45)
+  readonly name: string;
+
+  @IsString({ message: 'Password must be an string' })
+  @Min(8, { message: 'Password must be at least 8 characters long' })
+  @Max(255, { message: 'Password length must me less or equal 255' })
+  readonly password;
+}
