cd8eda92765f0da073f7e8d1c8dcee11b1d83b23
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-16 17:11:55 +0100

    Installed @nestjs-query/core and typeorm query

--- main-server/src/users/users.module.ts
+++ main-server/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserEntity} from "../entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService]
 })
