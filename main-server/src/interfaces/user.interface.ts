51e09db4cca868bc744275089d8d273a34431d21
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-16 17:46:26 +0100

    Bug fixing and created user interface :smile::two_hearts:

---
+++ main-server/src/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+export interface User {
+  readonly id: number;
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+  readonly created_at: Date;
+  readonly updated_at: Date;
+  readonly deleted_at?: Date;
+}
