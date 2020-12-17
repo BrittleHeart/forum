822e1eb850af6e675e16981a633416b5f3745ac1
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-17 10:13:20 +0100

    Created auth module

--- main-server/src/users/users.module.ts
+++ main-server/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/users/user.entity';
 import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
   controllers: [UsersController, AuthController],
   providers: [UsersService],
 })
