5ef6ab1afb8a03e231c39aa6c2ed5f487efa902d
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-17 09:13:01 +0100

    Added auth controller

--- main-server/src/users/users.module.ts
+++ main-server/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/users/user.entity';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [UsersController],
+  controllers: [UsersController, AuthController],
   providers: [UsersService],
 })
 export class UsersModule {}
