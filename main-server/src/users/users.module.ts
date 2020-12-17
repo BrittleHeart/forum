1d2d39df72c7b4a343caa35e9513443f57253354
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-17 10:15:29 +0100

    Created auth module

--- main-server/src/users/users.module.ts
+++ main-server/src/users/users.module.ts
@@ -10,5 +10,6 @@ import { AuthModule } from './auth/auth.module';
   imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
   controllers: [UsersController, AuthController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
