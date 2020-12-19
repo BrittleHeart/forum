7120174c691e0afdaf30f6f5fcb899673501c621
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-19 13:30:50 +0100

    Created profile interface

---
+++ main-server/src/interfaces/profile.interface.ts
@@ -0,0 +1,10 @@
+export interface ProfileInterface {
+  readonly avatar_url: string;
+  readonly first_name: string;
+  readonly last_name: string;
+  readonly city: string;
+  readonly homepage: string;
+  readonly info: string;
+  readonly created_at: Date;
+  readonly updated_at: Date;
+}
