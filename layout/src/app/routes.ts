4e628f2cd3184520230796105057ef5d5016e7f5
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-23 17:06:27 +0100

    Created routes file and top-navbar component

---
+++ layout/src/app/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: TopNavbarComponent,
+  },
+];
