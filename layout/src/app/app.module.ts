30b66900b1e4a3a8f3516dc09bbf60742af13ad4
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 15:42:20 +0100

    Created footer component

--- layout/src/app/app.module.ts
+++ layout/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 import { TopicComponent } from './topic/topic.component';
 import { TopicDetailsComponent } from './topic-details/topic-details.component';
 import { CategoryComponent } from './category/category.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [AppComponent, TopNavbarComponent, TopicComponent, TopicDetailsComponent, CategoryComponent],
+  declarations: [AppComponent, TopNavbarComponent, TopicComponent, TopicDetailsComponent, CategoryComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
