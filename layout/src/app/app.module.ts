0d735713c201bbf5532c12fc0b7e16e6f018109f
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 15:02:37 +0100

    Created category component

--- layout/src/app/app.module.ts
+++ layout/src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 import { TopicComponent } from './topic/topic.component';
 import { TopicDetailsComponent } from './topic-details/topic-details.component';
+import { CategoryComponent } from './category/category.component';
 @NgModule({
-  declarations: [AppComponent, TopNavbarComponent, TopicComponent, TopicDetailsComponent],
+  declarations: [AppComponent, TopNavbarComponent, TopicComponent, TopicDetailsComponent, CategoryComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
