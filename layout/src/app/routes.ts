19ef038d5ecd9112c882c2e78c45dd8065d88d10
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 14:45:23 +0100

    Added topics/topicId route

--- layout/src/app/routes.ts
+++ layout/src/app/routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TopicComponent } from './topic/topic.component';
+import { TopicDetailsComponent } from './topic-details/topic-details.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: TopicComponent,
+  },
+  {
+    path: 'topics/:topicId',
+    component: TopicDetailsComponent,
   },
 ];
