128b7a5fb076fa4f86b972f91b9738571bfa35e5
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 14:47:18 +0100

    Imported Topic Details Component

--- layout/src/app/app.module.ts
+++ layout/src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 import { TopicComponent } from './topic/topic.component';
+import { TopicDetailsComponent } from './topic-details/topic-details.component';
 @NgModule({
-  declarations: [AppComponent, TopNavbarComponent, TopicComponent],
+  declarations: [AppComponent, TopNavbarComponent, TopicComponent, TopicDetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
