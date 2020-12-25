23caa017ab680aaefb04b2fd26e8e7a556902363
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 15:41:07 +0100

    Styled categories template

--- layout/src/app/category/category.component.ts
+++ layout/src/app/category/category.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../interfaces/category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
+  categories: Category[] | Promise<Category[]>;
 
-  constructor() { }
+  constructor(private readonly categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.categories = this.categoryService.selectAllCategories();
   }
-
 }
