359e6ce276ef47a729f99836baf188a2d5a9e797
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 12:25:57 +0100

    Created prototype of topic component :rocket:

--- layout/src/app/topic/topic.component.ts
+++ layout/src/app/topic/topic.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Topic } from '../interfaces/topic';
+import { TopicsService } from '../topics.service';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.scss']
+  styleUrls: ['./topic.component.scss'],
 })
 export class TopicComponent implements OnInit {
+  public topics: Topic[] | Promise<Topic[]>;
 
-  constructor() { }
+  constructor(private readonly topicsService: TopicsService) {}
 
   ngOnInit(): void {
+    this.topics = this.topicsService.selectAllTopics();
   }
-
 }
