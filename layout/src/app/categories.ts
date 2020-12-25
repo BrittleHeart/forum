c39072d1271df08e2515db49e1411e2ceed8ab37
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-25 14:54:48 +0100

    Created category file

---
+++ layout/src/app/categories.ts
@@ -0,0 +1,20 @@
+import { Category } from './interfaces/category';
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    name: 'Trading Monkey',
+  },
+  {
+    id: 2,
+    name: 'Vault Keys Giveaway',
+  },
+  {
+    id: 3,
+    name: 'Misc Guns Locations',
+  },
+  {
+    id: 4,
+    name: 'Looking for Players',
+  },
+];
