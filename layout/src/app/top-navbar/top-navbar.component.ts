88165327126852c0ae6cc0d58a6d6af48eb8ec77
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-26 12:51:17 +0100

    Re-created navigation

--- layout/src/app/top-navbar/top-navbar.component.ts
+++ layout/src/app/top-navbar/top-navbar.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-top-navbar',
   templateUrl: './top-navbar.component.html',
-  styleUrls: ['./top-navbar.component.scss']
+  styleUrls: ['./top-navbar.component.scss'],
 })
 export class TopNavbarComponent implements OnInit {
+  faMenu = faHamburger;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
