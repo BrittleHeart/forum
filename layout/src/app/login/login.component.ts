e66cd8c1fedb49198b2d901807a4316cd2a93f8b
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-26 16:16:17 +0100

    Implemented prototype authentication

--- layout/src/app/login/login.component.ts
+++ layout/src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private readonly usersService: UsersService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(email: string, password: string): void {
+    const user = this.usersService.authenticate(email, password);
+    if (user) alert(`Email: ${user.email} Password: ${user.password}`);
   }
-
 }
---
+++ layout/src/app/services/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CrudService } from '../externals/crud-service';
+import { User } from '../interfaces/user';
+import { users } from '../users';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService extends CrudService<User> {
+  private users: User[] = users;
+
+  constructor() {
+    super();
+    this.collection = this.users;
+  }
+
+  findUser(email: string): User {
+    return this.collection.find((user: User) => user.email === email);
+  }
+
+  authenticate(email: string, password: string): User {
+    const user = this.findUser(email);
+    if (!user && password !== user.password)
+      alert('Error: Could not find user with this email');
+
+    return user;
+  }
+}
