6edc638d59470f437ff92b4e8a80dd4942e88225
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-26 14:26:14 +0100

    Created hard coded users information

---
+++ layout/src/app/topic-details/users.ts
@@ -0,0 +1,22 @@
+import { User } from '../interfaces/user';
+
+export const users: User[] = [
+  {
+    id: 1,
+    name: 'user test',
+    email: 'test@test.com',
+    password: 'test1',
+  },
+  {
+    id: 2,
+    name: 'user test 1',
+    email: 'test1@test.com',
+    password: 'test1',
+  },
+  {
+    id: 3,
+    name: 'user test 2',
+    email: 'test2@test.com',
+    password: 'test1',
+  },
+];
