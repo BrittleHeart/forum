04a925b54c9a90a29538b15b025aac8ebc697fe2
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-26 14:06:23 +0100

    Created prototype of navigation animate :sob:

--- layout/src/app/top-navbar/top-navbar.component.ts
+++ layout/src/app/top-navbar/top-navbar.component.ts
@@ -1,23 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import {
+  trigger,
+  state,
+  animate,
+  transition,
+  style,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-top-navbar',
   templateUrl: './top-navbar.component.html',
   styleUrls: ['./top-navbar.component.scss'],
+  animations: [
+    trigger('toggleNavigation', [
+      state(
+        'open',
+        style({
+          transform: 'translate(0, 0)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'translate(-50%, 0)',
+        })
+      ),
+      transition('open => closed', [
+        style({ transform: 'translate(-50%, 0)' }),
+        style({ transform: 'translate(0, 0)' }),
+        animate('1s ease-in-out'),
+      ]),
+      transition('closed => open', [animate('0.5s ease-in-out')]),
+    ]),
+  ],
 })
 export class TopNavbarComponent implements OnInit {
   faMenu = faHamburger;
+  public isOpen: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  /**
+   * Toggles burger menu styles
+   */
   toggleMenu() {
-    const menu = document.querySelector('#burgerMenu');
-    const overlay = document.querySelector('#overlay');
-
-    menu.classList.toggle('hidden');
-    overlay.classList.toggle('hidden');
+    this.isOpen = !this.isOpen;
   }
 }
