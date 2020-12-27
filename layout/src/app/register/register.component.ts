bee370bae08ef4f218ecb426a378b3fbd8b6216e
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-27 17:07:46 +0100

    Created hard coded register

--- layout/src/app/register/register.component.ts
+++ layout/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from '../services/users.service';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +11,36 @@ import { FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   errors: Array<string>;
+  formSent: boolean;
 
-  constructor() {}
+  constructor(private readonly usersService: UsersService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
 
-  register() {}
+    this.errors = [];
+    this.formSent = false;
+  }
+
+  register(): void {
+    const { name, email, password } = this.registerForm.controls;
+    const { collection } = this.usersService;
+    const user: User = {
+      id: collection.length + 1,
+      name: name.value,
+      email: email.value,
+      password: password.value,
+    };
+
+    this.usersService.add(user);
+
+    if (this.registerForm.valid) this.formSent = true;
+  }
 }
