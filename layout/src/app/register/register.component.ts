a27a172decf0d3c0ca187e4ec877ac98a0a58c75
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-27 16:13:21 +0100

    Created prototype of register styles

--- layout/src/app/register/register.component.ts
+++ layout/src/app/register/register.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  errors: Array<string>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  register() {}
 }
