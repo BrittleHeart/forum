3e1227ea5a37514bdf48daa4cd67409aac5c0d65
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-29 20:05:27 +0100

    Added temporary Observable types

--- layout/src/app/externals/crud-service-interface.ts
+++ layout/src/app/externals/crud-service-interface.ts
@@ -1,13 +1,14 @@
 import { AuthErrorInterface } from '../interfaces/auth-error-interface';
+import { Observable } from 'rxjs';
 
 export interface CrudServiceInterface<T> {
   collection: T[] | undefined;
   errors: AuthErrorInterface[] | undefined;
 
-  add(t: T): number | Promise<number>;
-  get(id: number): T | Promise<T>;
-  clear(): T[] | Promise<T[]>;
-  selectAll(): T[] | Promise<T[]>;
-  update(id: number): T | Promise<T> | T[] | Promise<T[]>;
-  delete(id: number): T[] | Promise<T[]>;
+  add(t: T): number | Promise<number> | Observable<number>;
+  get(id: number): T | Promise<T> | Observable<T>;
+  clear(): T[] | Promise<T[]> | Observable<T[]>;
+  selectAll(): T[] | Promise<T[]> | Observable<T[]>;
+  update(id: number): T | Promise<T> | T[] | Promise<T[]> | Observable<T>;
+  delete(id: number): T[] | Promise<T[]> | Observable<T>;
 }
--- layout/src/app/externals/crud-service.ts
+++ layout/src/app/externals/crud-service.ts
@@ -1,5 +1,6 @@
 import { CrudServiceInterface } from './crud-service-interface';
 import { AuthErrorInterface } from '../interfaces/auth-error-interface';
+import { Observable } from 'rxjs';
 
 export abstract class CrudService<T> implements CrudServiceInterface<T> {
   public collection: T[] | undefined;
@@ -9,25 +10,25 @@ export abstract class CrudService<T> implements CrudServiceInterface<T> {
     return this.collection.push(t);
   }
 
-  clear(): T[] | Promise<T[]> {
+  clear(): T[] | Promise<T[]> | Observable<T[]> {
     this.collection = [];
     return this.collection;
   }
 
-  get(id: number): Promise<T> | T {
+  get(id: number): Promise<T> | T | Observable<T> {
     // @ts-ignore
     return this.collection.find((item: T) => item.id === id);
   }
 
-  selectAll(): T[] | Promise<T[]> {
+  selectAll(): T[] | Promise<T[]> | Observable<T[]> {
     return this.collection;
   }
 
-  delete(id: number): T[] | Promise<T[]> {
+  delete(id: number): T[] | Promise<T[]> | Observable<T> {
     return this.collection.splice(id, 1);
   }
 
-  update(id: number): T[] | Promise<T[]> {
+  update(id: number): T[] | Promise<T[]> | Observable<T> {
     const collection_copy = [...this.collection];
     if (!collection_copy) return;
     return collection_copy;
