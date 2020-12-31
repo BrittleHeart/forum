04ed8c2b6b4b21a3420f809217aaf9ce05e7332b
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-31 10:51:25 +0100

    Created prototype of topic details

--- layout/src/app/topic-details/topic-details.component.ts
+++ layout/src/app/topic-details/topic-details.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Topic } from '../interfaces/topic';
+import { topics } from '../topics';
 
 @Component({
   selector: 'app-topic-details',
   templateUrl: './topic-details.component.html',
-  styleUrls: ['./topic-details.component.scss']
+  styleUrls: ['./topic-details.component.scss'],
 })
 export class TopicDetailsComponent implements OnInit {
+  topic: Topic;
 
-  constructor() { }
+  constructor(private readonly route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const topicIdFromRoute = this.route.snapshot.paramMap.get('topicId');
+    this.topic = topics.find(
+      (topic: Topic) => topic.id === Number(topicIdFromRoute)
+    );
   }
-
 }
