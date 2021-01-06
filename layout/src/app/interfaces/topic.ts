9b85ef91c9f4581153483456926787a53ac412bc
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-06 14:24:02 +0100

    Modified topics array

--- layout/src/app/interfaces/topic.ts
+++ layout/src/app/interfaces/topic.ts
@@ -1,6 +1,9 @@
+import { Comment } from './comment';
+
 export interface Topic {
   readonly id: number;
   readonly title: string;
   readonly description: string;
   readonly content: string;
+  readonly comments?: Comment[];
 }
--- layout/src/app/topics.ts
+++ layout/src/app/topics.ts
@@ -8,6 +8,19 @@ export const topics: Topic[] = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis, magna quis ultricies efficitur, metus orci pulvinar metus, in dapibus magna odio vel sapien. Sed auctor ligula porttitor neque tincidunt, id tristique nisi viverra. Aenean a tortor eget eros sodales malesuada. Praesent ornare, sem eget tempor fermentum, nisi lorem maximus erat, nec interdum quam lectus ut mauris. Praesent in justo leo. Integer eu sem eros. ',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis, magna quis ultricies efficitur, metus orci pulvinar metus, in dapibus magna odio vel sapien. Sed auctor ligula porttitor neque tincidunt, id tristique nisi viverra. Aenean a tortor eget eros sodales malesuada. Praesent ornare, sem eget tempor fermentum, nisi lorem maximus erat, nec interdum quam lectus ut mauris. Praesent in justo leo. Integer eu sem eros. Mauris consequat mattis dolor. Donec fringilla mauris eget nisi scelerisque aliquam. In hac habitasse platea dictumst. Aliquam nisi nisi, eleifend vel est lobortis, blandit rhoncus lacus. Aliquam arcu nisl, bibendum vitae blandit ut, mattis ac quam. Nam posuere, odio a dignissim suscipit, mi ante rhoncus neque, sed mattis quam justo at sapien. Nullam a lorem condimentum, imperdiet lorem non, placerat nulla. ',
+    comments: [
+      {
+        user: {
+          id: 1,
+          email: 'test@test.com',
+          name: 'Test',
+          password: 'test1',
+        },
+        title: 'Not that bad',
+        content:
+          "Well, that's quite nice. For me, this topic is really helpful",
+      },
+    ],
   },
   {
     id: 2,
